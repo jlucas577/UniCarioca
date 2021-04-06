f03210a7b79b5236a42cca93766c6782da555269
jlucas577 <lucas-martinsbr@hotmail.com>
2021-04-06 10:46:23 -0300

    add translate and new files of av1

--- C/HelloWorld.c
+++ C/HelloWorld.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 /*
-	Esse simples código tem como principal função escrever um
+	en_US: This simple code has the main function of writing a
+	Hello World on the user's console screen using
+	C as a development language.
+*/
+
+/*
+	pt_BR: Esse simples código tem como principal função escrever um
 	Hello World na tela do console do usuário utilizando
 	C como linguagem de desenvolvimento.
 */
--- C/Triangle.c
+++ C/Triangle.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 /*
-	Esse algoritmo recebe o valor de X, Y e Z e tem como objetivo verificar se
+	en_US: This algorithm receives the value of X, Y and Z and aims to verify whether
+	they can be the lengths of the sides of a triangle. Showing to user
+	a message with the result of this check.
+	Note: The length of each side of a triangle must be less than the sum
+	the lengths of the other two sides.
+*/
+
+/*
+	pt_BR: Esse algoritmo recebe o valor de X, Y e Z e tem como objetivo verificar se
 	eles podem ser os comprimentos dos lados de um triângulo. Exibindo ao usuário
 	uma mensagem com o resultado dessa verificação.
 	Obs.: O comprimento de cada lado de um triângulo deve ser menor do que a soma
